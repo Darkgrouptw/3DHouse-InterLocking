48a9c25af39421b3f7dc3087a405c585ca133abc
Dark <darkgrouptw@gmail.com>
2016-05-16 22:52:20 +0800

    能讀 OBJ 檔案

--- House InterLocking/main.cpp
+++ House InterLocking/main.cpp
@@ -1,7 +1,79 @@
 #include "main.h"
 
+void ReadFile(char* inputFile)
+{
+	QString tempStr = QString(inputFile);
+	QFile file(tempStr);
+	if (!file.open(QIODevice::ReadOnly))
+	{
+		qDebug() << "Input File can't open !!";
+		return;
+	}
+	QTextStream ss(&file);
+	int tempInt,totalNumber;
+	float tempFloat, tempX, tempY, tempZ, tempType;
+
+	QString InputFileStr(inputFile);
+	QStringList FilePathList = InputFileStr.split("/");
+	InputFileStr = "";
+	for (int i = 0; i < FilePathList.length() - 1; i++)
+		InputFileStr += FilePathList[i] + "/";
+
+	ss >> totalNumber;
+	qDebug() << "Total Number => " << totalNumber;
+	for (int i = 0; i < totalNumber; i++)
+	{
+		MyMesh *tempMesh = new MyMesh();
+		InterLockingInfo *info = new InterLockingInfo();
+		ss >> tempStr;
+		OpenMesh::IO::read_mesh(*tempMesh, (InputFileStr + tempStr).toLocal8Bit().data());
+		tempStr.replace("model_part", "");
+		info->PartNumber = tempStr.toInt();
+		qDebug() << "Number of part => " << info->PartNumber;
+
+		tempMesh->request_vertex_status();
+		tempMesh->request_edge_status();
+		tempMesh->request_face_status();
+		Model.push_back(tempMesh);
+
+		ss >> info->PartName;
+		ss >> tempInt;
+		for (int j = 0; j < tempInt / 2; j++)
+			for (int k = 0; k < 6; k++)
+			{
+				ss >> tempX >> tempY >> tempZ >> tempY >> tempType;
+				switch (k)
+				{
+				case 0:
+					info->InterLockType = tempType;
+					info->InterLockingFace.push_back(MyMesh::Point(tempX, tempY, tempZ));
+					break;
+				case 1:
+				case 2:
+				case 5:
+					info->InterLockingFace.push_back(MyMesh::Point(tempX, tempY, tempZ));
+					break;
+				}
+			}
+	}
+	file.close();
+}
+
+
+
+void AddInterLocking()
+{
+
+}
+
 int main(int argc, char *argv[])
 {
-	system("pause");
+	if (argc == 2)
+	{
+		ReadFile(argv[1]);
+		AddInterLocking();
+	}
+	else
+		qDebug() << "Please add file path !!";
 	return 0;
 }
--- House InterLocking/main.h
+++ House InterLocking/main.h
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <QDebug>
+#include <QFile>
+#include <QVector>
+#include <QIODevice>
+#include <QTextStream>
 
 #include <OpenMesh/Core/IO/MeshIO.hh>
 #include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
 
 typedef OpenMesh::PolyMesh_ArrayKernelT<> MyMesh;
+
+struct InterLockingInfo
+{
+	int PartNumber;												// 第幾塊
+	int InterLockType;											// 0 跟 1
+	QString PartName;											// 是地板，左牆還是右牆
+	QVector<MyMesh::Point> InterLockingFace;					// 連接的面
+};
+
+QVector<InterLockingInfo>	InfoVector;							// 存接起來面
+QVector<MyMesh *>			Model;								// 所有的 model
+
+void ReadFile(char *);
+void AddInterLocking();
